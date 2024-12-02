62b811449456e2238d6e48828dd577224d2038e5
inyong <inyong2050@gmail.com>
2024-12-02 11:22:31 +0900

    add-inyong

---
+++ inyong/variables.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  description = "The region where the resources are created."
+  default     = "ap-northeast-2"
+}
+
+variable "user-id" {
+  description = "User ID for restrict"
+  type = string
+  nullable = false
+
+  validation {
+    condition = !contains(["*"], var.user-id)
+    error_message = "Do not use *"
+  }
+}
+
+variable "session-time" {
+  description = ""
+  type = string
+  nullable = false
+}
