b0cab4bdffb3c79046ff936ce6e40654707fca80
inyong <inyong2050@gmail.com>
2024-12-02 11:33:18 +0900

    add-inyong

--- inyong/data.tf
+++ inyong/data.tf
@@ -16,7 +16,7 @@ data "aws_iam_policy_document" "cac-policy" {
       variable = "aws:userid"
       values   = ["*:${var.user-id}"]
     }
-#
+
     condition {
       test     = "DateLessThan"
       variable = "aws:CurrentTime"
