db3d01524a1a3243ddc5be107dcc76dcdf0c0f26
inyong <inyong2050@gmail.com>
2024-12-09 15:55:04 +0900

    module

--- terraform-aws-common/common/variable.tf
+++ terraform-aws-common/common/variable.tf
@@ -1,148 +1,70 @@
-#############
-## TAG
-#############
-variable "env" {
-  type        = string
-  default     = "prd"
-  description = "인프라 환경 정의 dev | stg | prd"
+variable "region" {
+  description = "The region where the resources are created."
+  default     = "ap-northeast-2"
+}
+
+variable "user-id" {
+  description = "User ID for restrict"
+  type = string
+  nullable = false
 
   validation {
-    condition     = contains(["dev", "stg", "prd"], var.env)
-    error_message = "환경 정의는 dev, stg, prd가 가능"
+    condition = !contains(["*"], var.user-id)
+    error_message = "Do not use *"
   }
 }
 
-variable "pjt" {
-  type        = string
-  description = "프로젝트 이름"
-}
-
-#############
-## NETWORK
-#############
-variable "vpc_cidr" {
-  type        = string
-  default     = "100.64.0.0/16"
-  description = "VPC의 CIDR 정의, 16비트 대역을 입력 (e.g. 100.64.0.0/16)"
+variable "duration" {
+  description = "session duration"
+  default = 2
+  type = number
+  nullable = false
 
   validation {
-    condition     = contains(split("/", var.vpc_cidr), "16")
-    error_message = "CIDR은 16비트"
+    condition = (var.duration) <= 2
+    error_message = "Do not use *"
   }
 }
 
-variable "sg_bastion_egress" {
-  type        = map(tuple([string, list(string)]))
-  default     = {}
-  description = "Bastion 서버의 Security Group Egress 목록"
+variable "action" {
+  description = "define action"
+  type = string
+  nullable = false
+
+  validation {
+    condition = !contains(["*"], var.action)
+    error_message = "Do not use *"
+  }
 }
 
-variable "sg_bastion_ingress" {
-  type        = map(tuple([string, list(string)]))
-  default     = {}
-  description = "Bastion 서버의 Security Group Ingress 목록"
+variable "cmdb-role" {
+  description = "define cmdb-role"
+  type = string
+  nullable = false
 }
 
-#############
-## AMI
-#############
-variable "ami_ownerid" {
-  default = ""
+variable "perms_s3" {
+  description = "s3-permissions"
+  type = list(object({
+    s3_bucket_arn = string
+    s3_access_point_arn = string
+  }))
+  nullable = false
+  default = []
 }
 
-variable "ami_env" {
-  default = "prod"
+variable "resource_arn" {
+  description = "s3-resource-arn"
+  type = list(object({
+    s3_bucket_arn = string
+    s3_access_point_arn = string
+  }))
+  default = []
 }
 
-#############
-## EKS
-#############
-variable "enable_eks" {
-  type    = bool
-  default = false
-}
-
-variable "sg_eks_cluster_egress" {
-  type        = map(tuple([string, list(string)]))
-  default     = {}
-  description = "EKS 클러스터의 Security Group Egress 목록"
-}
-
-variable "sg_eks_cluster_ingress" {
-  type        = map(tuple([string, list(string)]))
-  default     = {}
-  description = "EKS 클러스터의 Security Group Ingress 목록"
-}
-
-variable "sg_eks_node_egress" {
-  type        = map(tuple([string, list(string)]))
-  default     = {}
-  description = "EKS 노드의 Security Group Egress 목록"
-}
-
-variable "sg_eks_node_ingress" {
-  type        = map(tuple([string, list(string)]))
-  default     = {}
-  description = "EKS 노드의 Security Group Ingress 목록"
-}
-
-variable "sg_eks_pod_egress" {
-  type        = map(tuple([string, list(string)]))
-  default     = {}
-  description = "EKS 파드의 Security Group Egress 목록"
-}
-
-variable "sg_eks_pod_ingress" {
-  type        = map(tuple([string, list(string)]))
-  default     = {}
-  description = "EKS 파드의 Security Group Ingress 목록"
-}
-
-#############
-## Elasticache
-#############
-variable "enable_elasticache" {
-  type        = bool
-  default     = false
-  description = "Elasticache 활성/비활성"
-}
-
-variable "sg_elasticache_egress" {
-  type        = map(tuple([string, list(string)]))
-  default     = {}
-  description = "Elasticache의 Security Group Egress 목록"
-}
-
-variable "sg_elasticache_ingress" {
-  type        = map(tuple([string, list(string)]))
-  default     = {}
-  description = "Elasticache의 Security Group Ingress 목록"
-}
-
-#####################
-## dynamodb
-#####################
-variable "enable_dynamodb" {
-  type        = bool
-  default     = false
-  description = "DynamoDB 활성/비활성"
-}
-
-#####################
-## iam - users per groups
-#####################
-
-variable "CloudArchitectureTeam_users" {
-  description = "Create IAM CloudArchitectureTeam Users"
-  type        = list(any)
-}
-
-variable "developer_group_users" {
-  description = "Create IAM Developer Group Users"
-  type        = list(any)
-}
-
-variable "key_pair_name" {
-  type        = string
-  description = "Key pair 이름 정의"
-}
+# variable "session-time" {
+#   description = ""
+# #  default = timeadd(timestamp(), var.duration)
+#   type = string
+#   nullable = false
+# }
